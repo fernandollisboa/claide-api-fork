9002a9326098e4b1fb2b906c3e239f3f8cab8f06
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2022-10-24 20:29:16 -0300

    chore: add cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
-import express, { json } from "express";
+import cors from "cors";
+import express from "express";
 import routes from "./routes/index.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+
 const app = express();
-
-const port = 4000; 
-
-app.use(json());
-
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 app.listen(port, () => {
