1c0584dae3846b99f78c6d83d5dc63265562cd85
Caetano Albuquerque <caetano.albuquerque@lsd.ufcg.edu.br>
2022-11-10 19:19:31 -0300

    feat(projects and members auth): adding authentication to all members and projects routes

--- src/routes/projectsRouter.js
+++ src/routes/projectsRouter.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import * as projectController from "../controllers/projectController";
+import auth from "../middlewares/auth";
 
 const projectsRouter = Router();
 
-projectsRouter.post("/", projectController.createProject);
-projectsRouter.get("/", projectController.getProjects);
-projectsRouter.get("/:id", projectController.getProjectById);
-
-projectsRouter.put("/", projectController.updateProject);
+projectsRouter.post("/", auth, projectController.createProject);
+projectsRouter.get("/", auth, projectController.getProjects);
+projectsRouter.get("/:id", auth, projectController.getProjectById);
+projectsRouter.put("/", auth, projectController.updateProject);
 
 export default projectsRouter;
