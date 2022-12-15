a343e94df96714e7eff91810b1a0a23115322fdf
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2022-12-14 22:20:23 -0300

    fix: correct small typo on serviceTest

--- tests/unit/service.test.js
+++ tests/unit/service.test.js
@@ -4,7 +4,7 @@ import { faker } from "@faker-js/faker";
 
 import * as servicesService from "../../src/services/servicesService";
 import * as serviceRepository from "../../src/repositories/serviceRepository";
-import ServiceNotFoundError from "../../src/errors/serviceNotFoundError";
+import ServiceNotFoundError from "../../src/errors/ServiceNotFoundError";
 import { createValidService, createValidServiceWithId } from "../factories/serviceFactory";
 
 describe("services service", () => {
