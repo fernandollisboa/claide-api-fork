33663e5714e6fe18001a82f1bdaf33d446be998e
Caetano Albuquerque <caetanoalbuquerque@pop-os.localdomain>
2023-01-04 00:52:32 -0300

    docs(docs): updating the docker files and the guide to deploy it

--- src/setup.js
+++ src/setup.js
@@ -6,6 +6,8 @@ if (process.env.NODE_ENV === "PROD") {
   envFile = ".env.prod";
 } else if (process.env.NODE_ENV === "TEST") {
   envFile = ".env.test";
+} else if (process.env.NODE_ENV === "DEV") {
+  envFile = ".env.dev";
 } else {
   envFile = ".env";
 }
