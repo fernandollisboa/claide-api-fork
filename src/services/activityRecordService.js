9d7f15da95c15eef3864052f44f63ab5d37b05b7
Caio Davi Pereira da Silva <caio.silva@ccc.ufcg.edu.br>
2023-01-29 04:22:18 -0300

    refactor(activity record): save only attributes that was changed on update in activityRecord

--- src/services/activityRecordService.js
+++ src/services/activityRecordService.js
@@ -1,10 +1,23 @@
 import * as activityRepository from "../repositories/activityRecordRepository";
+import { getDifference } from "../utils/activityRecordUtils";
 
 export async function createActivity(activity) {
+	let {oldValue,newValue} = activity;
+	
+	if(oldValue && newValue){
+		const {oldValueChanged,newValueChanged} = getDifference(oldValue,newValue);		
+		oldValue = oldValueChanged;
+		newValue = newValueChanged;
+	}
+
 	const newActivity = {
 		...activity,
+		oldValue,
+		newValue,
 		date: new Date(),
 	};
+	console.log("oieee");
+	console.log(newActivity);
 
 	return await activityRepository.insertActivity(newActivity);
 }
