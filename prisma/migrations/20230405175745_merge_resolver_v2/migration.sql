bc6cd8477195fd13125d562995332a7f13a34c9f
Fernando Lisboa <55773308+fernandollisboa@users.noreply.github.com>
2023-04-11 10:33:41 -0300

    hotfix/delete broken migration (#70)
    * test: create first integration test
    
    * test: refine member crud tests
    
    * fix: delete broken migration

--- prisma/migrations/20230405175745_merge_resolver_v2/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "RegistrationStatus" ADD CONSTRAINT "RegistrationStatus_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "members"("username") ON DELETE RESTRICT ON UPDATE CASCADE;
